e293cfa789fea87fced05c35fd053c2214ba963e
Maiara Moraes <maiara.ctt@gmail.com>
2024-10-26 11:26:29 -0300

    feat: classe que armazena o desempenho de cada algoritmo de ordenação

--- SortingAlgorithm/src/SortingMetrics.java
+++ SortingAlgorithm/src/SortingMetrics.java
@@ -1,3 +1,23 @@
 public class SortingMetrics {
-    
+    private long runtime;
+    private int comparisons;
+    private int movements;
+
+    public SortingMetrics(long runtime, int comparisons, int movements) {
+        this.runtime = runtime;
+        this.comparisons = comparisons;
+        this.movements = movements;
+    }
+
+    public long getRuntime() {
+        return runtime;
+    }
+
+    public int getComparisons() {
+        return comparisons;
+    }
+
+    public int getMovements() {
+        return movements;
+    }
 }
