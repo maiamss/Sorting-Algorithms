cfe640e238205352a9b8741cfff84ba788afada1
Maiara Moraes <maiara.ctt@gmail.com>
2024-10-26 11:24:13 -0300

    feat: classe counter

---
+++ SortingAlgorithm/src/Counter.java
@@ -0,0 +1,20 @@
+public class Counter {
+    private int comparisons = 0;
+    private int movements = 0;
+
+    public void incrementComparisons() {
+        comparisons++;
+    }
+
+    public void incrementMovements(int count) {
+        movements += count;
+    }
+
+    public int getComparisons() {
+        return comparisons;
+    }
+
+    public int getMovements() {
+        return movements;
+    }
+}
